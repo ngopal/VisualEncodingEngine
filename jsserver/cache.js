53127d6896877e6c6ed643b85f1425a5a796dc41
Nikhil Gopal <nikhilgopal@gmail.com>
2016-08-03 18:49:54 -0700

    debugged cache functions

--- jsserver/cache.js
+++ jsserver/cache.js
@@ -11,25 +11,54 @@ function initializeCache() {
         sessionStorage.setItem('thanks', 'incomplete');
     }
     else {
-        if ( sessionStorage.getItem('consent') === 'incomplete' ) {
+        if ( window.location.href.split('/')[3] !== "consent" && sessionStorage.getItem('consent') === 'incomplete' ) {
             window.location.href="/consent";
         }
+        else if ( window.location.href.split('/')[3] === "consent" && sessionStorage.getItem('consent') === 'incomplete' ) {
+            console.log("Need to obtain consent");
+        }
+        else if ( window.location.href.split('/')[3] === "consent" && sessionStorage.getItem('consent') === 'complete' ) {
+            window.location.href='/tutorial'
+        }
         else {
-            if ( sessionStorage.getItem('tutorial') === 'incomplete' ) {
-                window.location.href="/tutorial";
+            if ( window.location.href.split('/')[3] !== "tutorial" && sessionStorage.getItem('tutorial') === 'incomplete' ) {
+                window.location.href="/consent";
+            }
+            else if ( window.location.href.split('/')[3] === "tutorial" && sessionStorage.getItem('tutorial') === 'incomplete' ) {
+                console.log("Need to complete tutorial");
+            }
+            else if ( window.location.href.split('/')[3] === "tutorial" && sessionStorage.getItem('tutorial') === 'complete' ) {
+                window.location.href=nextRandomPage();
             }
             else {
                 if ( sessionStorage.getItem('status') === 'incomplete' ) {
                     //window.location.href="/";
                     console.log("experiment in progress");
+                    //check beento here
+                    var currentPageVal = getPageVal();
+                    var beento = JSON.parse('['+sessionStorage.getItem('beento')+']');
+                    if (beento.indexOf(currentPageVal) !== -1) {
+                        var nextpage = nextRandomPage();
+                        console.log("SENT TO  "+nextpage);
+                        window.location.href = nextpage;
+                    }
                 }
                 else {
-                    if ( sessionStorage.getItem('demographic') === 'incomplete' ) {
-                        window.location.href="/demographic";
+                    if ( window.location.href.split('/')[3] !== "demographic" && sessionStorage.getItem('demographic') === 'incomplete' ) {
+                        window.location.href="/tutorial";
+                    }
+                    else if ( window.location.href.split('/')[3] === "demographic" && sessionStorage.getItem('demographic') === 'incomplete' ) {
+                        console.log("Need to complete demographic survey");
+                    }
+                    else if ( window.location.href.split('/')[3] === "demographic" && sessionStorage.getItem('demographic') === 'complete' ) {
+                        window.location.href='/thanks';
                     }
                     else {
-                        if ( sessionStorage.getItem('thanks') === 'incomplete' ) {
-                            window.location.href="/thanks";
+                        if ( window.location.href.split('/')[3] !== "thanks" && sessionStorage.getItem('thanks') === 'incomplete' ) {
+                            window.location.href="/demographic";
+                        }
+                        else if ( window.location.href.split('/')[3] === "thanks" && sessionStorage.getItem('thanks') === 'incomplete' ) {
+                            console.log("Need to thank participant");
                         }
                     }
                 }
@@ -39,10 +68,10 @@ function initializeCache() {
 }
 
 function thissession(n) {
-    initializeCache();
+    //initializeCache();
 
     if ( sessionStorage.getItem('status') === "complete" ) {
-        alert('load next page');
+        window.location.href='/demographic';
     }
 
     var pageval = n;
@@ -60,3 +89,27 @@ function thissession(n) {
         //sessionStorage.clear(); clear storage on exit page;
     }
 };
+
+function getRandomIntInclusive(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function nextRandomPage() {
+    var togoto = JSON.parse('['+sessionStorage.getItem('togoto')+']');
+    if (togoto.length === 0) {
+        return '/demographic';
+    }
+    var randsel = togoto[getRandomIntInclusive(0,togoto.length-1)];
+    return '/page'+randsel;
+}
+
+function getPageVal() {
+    var pageval;
+    if (window.location.href.split('/page')[1] === undefined) {
+        pageval = 1;
+    }
+    else {
+        pageval = parseInt(window.location.href.split('/page')[1]);
+    }
+    return pageval;
+}
